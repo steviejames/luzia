85efc858ac8eccbbeb63c26a23ae1037323f6acc
Sílvio Dumba <silviojava2@gmail.com>
2024-01-06 02:53:50 +0100

    added config

--- index.js
+++ index.js
@@ -13,27 +13,7 @@ const port = process.env.PORT ? Number(process.env.PORT) : 3030;
 const client = new Client({
   puppeteer: {
     args: [
-      "--disable-web-security",
       "--no-sandbox",
-      "--disable-web-security",
-      "--aggressive-cache-discard",
-      "--disable-cache",
-      "--disable-application-cache",
-      "--disable-offline-load-stale-cache",
-      "--disk-cache-size=0",
-      "--disable-background-networking",
-      "--disable-default-apps",
-      "--disable-extensions",
-      "--disable-sync",
-      "--disable-translate",
-      "--hide-scrollbars",
-      "--metrics-recording-only",
-      "--mute-audio",
-      "--no-first-run",
-      "--safebrowsing-disable-auto-update",
-      "--ignore-certificate-errors",
-      "--ignore-ssl-errors",
-      "--ignore-certificate-errors-spki-list",
     ],
   },
 });
